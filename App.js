3f8408027b41eaf054093ff73959685556ad3b9d
unknown <2312130113@ACADEMICO.IESB.BR>
2025-03-21 09:40:58 -0300

    Adicionar

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import { rotulo_input_meta, rotulo_btn_cadastro_meta, rotulo_lista_metas } from './mensagens';
 
 export default function App() {
   return (
     <View style={styles.mainContainer}>
-      <TextInput placeholder='rotulo_input_meta'></TextInput>
-      <Button title='rotulo_btn_cadastro_meta'></Button>
-      <Text>rotulo_lista_metas</Text>
+      <TextInput placeholder={rotulo_input_meta}></TextInput>
+      <Button title={rotulo_btn_cadastro_meta}></Button>
+      <Text>{rotulo_lista_metas}</Text>
     </View>
   );
 }
