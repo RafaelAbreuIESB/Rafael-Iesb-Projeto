100666a3f01431e81b9edab80a35b823819942ff
unknown <2312130113@academico.iesb.br>
2025-03-21 08:53:21 -0300

    Adicionar

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { rotulo_input_meta, rotulo_btn_cadastro, rotulo_lista_metas } from './mensagens';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <text>rotulo_input_meta</text>
+      <text>rotulo_btn_cadastro</text>
+      <text>rotulo_lista_metas</text>
       <StatusBar style="auto" />
     </View>
   );
