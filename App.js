6e221f3cf7e7b4f0f1fce3a78f517c3ae993169b
unknown <2312130113@ACADEMICO.IESB.BR>
2025-03-21 10:26:33 -0300

    Adicionar

--- App.js
+++ App.js
@@ -1,25 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
-import { rotulo_input_meta, rotulo_btn_cadastro_meta, rotulo_lista_metas } from './mensagens';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  rotulo_input_meta,
+  rotulo_btn_cadastro_meta,
+  rotulo_lista_metas,
+} from "./mensagens";
 
 export default function App() {
   return (
     <View style={styles.mainContainer}>
-      <TextInput placeholder={rotulo_input_meta}></TextInput>
-      <Button title={rotulo_btn_cadastro_meta}></Button>
-      <Text>{rotulo_lista_metas}</Text>
+      <view style={{ width: 150 }}>
+        <TextInput
+          style={styles.inputText}
+          placeholder={rotulo_input_meta}
+        ></TextInput>
+      </view>
+      <view style={{ width: 150 }}>
+        <Button title={rotulo_btn_cadastro_meta}></Button>
+      </view>
+      <view>
+        <Text>{rotulo_lista_metas}</Text>
+      </view>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  mainContainer:{
-    padding: 30
+  mainContainer: {
+    padding: 30,
   },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputText: {
+    borderColor: "#FF0000",
+    borderWidth: 1.5,
   },
 });
